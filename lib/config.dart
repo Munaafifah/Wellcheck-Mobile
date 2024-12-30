0896ab66c386d1af80cd7a15bf914270386c6201
DanialBrah <danialx003@gmail.com>
2024-12-30 13:23:59 +0800

    payment

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,9 @@
 class Config {
-  static const String baseUrl = "http://10.0.2.2:5000";
-  /*before android emulator
-  http://localhost:5000*/
+  static const String baseUrl = "http://localhost:5000";
+  /*before windows emulator
+  http://localhost:5000
+  
+  for mobile emulator
+  http://10.0.2.2:5000
+  */
 }
